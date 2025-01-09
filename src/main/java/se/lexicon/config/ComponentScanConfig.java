71cf59574b8f4e168e832d915ee2bfe86d5dfa54
Sayana Surendran <sayanasurendran31@gmail.com>
2025-01-09 13:28:05 +0100

    Part 2-   added error handling for scanner int

--- src/main/java/se/lexicon/config/ComponentScanConfig.java
+++ src/main/java/se/lexicon/config/ComponentScanConfig.java
@@ -3,6 +3,8 @@ package se.lexicon.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import se.lexicon.util.ScannerInputService;
+import se.lexicon.util.UserInputService;
 
 import java.util.Scanner;
 
@@ -13,6 +15,11 @@ public class ComponentScanConfig {
     @Bean
      public Scanner scanner(){
         return new Scanner(System.in);
+    }
+
+    @Bean
+    public ScannerInputService scannerInputService(){
+        return new ScannerInputService(scanner());
     }
 
 }

464383ab22829c78d1a1c1094ca2b3bf86b8d92a
Sayana Surendran <sayanasurendran31@gmail.com>
2025-01-09 11:11:41 +0100

    Part 2-   added ScannerInputService files and injected the dependencies

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import se.lexicon.config.ComponentScanConfig;
 import se.lexicon.data_access.StudentDao;
 import se.lexicon.data_access.StudentDaoListImpl;
+import se.lexicon.util.ScannerInputService;
+import se.lexicon.util.UserInputService;
 
 
 public class Main {
@@ -11,6 +13,7 @@ public class Main {
 
         AnnotationConfigApplicationContext context=new AnnotationConfigApplicationContext(ComponentScanConfig.class);
         StudentDao studentDao=context.getBean(StudentDaoListImpl.class);
+        UserInputService userInputService=context.getBean(ScannerInputService.class);
 
 
     }
---
+++ src/main/java/se/lexicon/util/ScannerInputService.java
@@ -0,0 +1,31 @@
+package se.lexicon.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Scanner;
+
+
+@Component
+public class ScannerInputService implements UserInputService{
+
+
+    private final Scanner scanner;
+
+    @Autowired
+    public ScannerInputService(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    @Override
+    public String getString() {
+        System.out.print("Enter a string: ");
+        return scanner.nextLine();
+    }
+
+    @Override
+    public int getInt() {
+        System.out.print("Enter an integer: ");
+         return scanner.nextInt();
+    }
+}

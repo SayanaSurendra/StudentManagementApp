4443a66c4b84f969fa52a31e143dc3aff69199a3
Sayana Surendran <sayanasurendran31@gmail.com>
2025-01-09 13:34:19 +0100

    update configuration file

--- src/main/java/se/lexicon/config/ComponentScanConfig.java
+++ src/main/java/se/lexicon/config/ComponentScanConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import se.lexicon.util.ScannerInputService;
-import se.lexicon.util.UserInputService;
+
 
 import java.util.Scanner;
 

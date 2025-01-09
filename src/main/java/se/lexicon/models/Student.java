311c3a9d2461820124f4581e7d35ff0209bf24c3
Sayana Surendran <sayanasurendran31@gmail.com>
2025-01-09 10:23:07 +0100

    Part 1- added Dao and implementation , spring configuration file, model class

--- src/main/java/se/lexicon/models/Student.java
+++ src/main/java/se/lexicon/models/Student.java
@@ -1,5 +1,7 @@
 package se.lexicon.models;
 
+import java.util.Objects;
+
 public class Student {
      private int id;
      private String name;
@@ -27,4 +29,25 @@ public class Student {
     }
 
 
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Student{");
+        sb.append("id=").append(id);
+        sb.append(", name='").append(name).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return getId() == student.getId() && Objects.equals(getName(), student.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName());
+    }
 }
